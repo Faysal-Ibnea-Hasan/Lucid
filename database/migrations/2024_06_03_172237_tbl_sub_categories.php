1626063ce5e7513ebc105191b23f7a15d4434887
Jesan <foysal.jesan@gmail.com>
2024-06-06 11:19:59 +0600

    Sub Category

--- database/migrations/2024_06_03_172237_tbl_sub_categories.php
+++ database/migrations/2024_06_03_172237_tbl_sub_categories.php
@@ -11,7 +11,15 @@ return new class extends Migration
      */
     public function up(): void
     {
-        //
+        Schema::create('tbl_sub_categories', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('image');
+            $table->string('image_URL');
+            $table->string('subCategory_Id');
+            $table->string('category_Id');
+            $table->timestamps();
+        });
     }
 
     /**
@@ -19,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //
+        Schema::dropIfExists('tbl_sub_categories');
     }
 };
